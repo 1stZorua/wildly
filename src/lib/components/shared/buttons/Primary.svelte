1efa0067b4bcbaff945b2ebe9195101602bcfb23
Zorua <milandehaart@live.nl>
2024-10-09 19:40:32 +0200

    Added ButtonPrimary component

---
+++ src/lib/components/shared/buttons/Primary.svelte
@@ -0,0 +1,16 @@
+<script lang=ts>
+	import type { ClassType } from "$components/_types";
+	import type { Snippet } from "svelte";
+	import Button from "./Button.svelte";
+
+    let { 
+        className, 
+        children 
+    }: { className?: ClassType, children?: Snippet } = $props();
+</script>
+
+<Button
+    {className}
+>
+    {@render children?.()}
+</Button>
