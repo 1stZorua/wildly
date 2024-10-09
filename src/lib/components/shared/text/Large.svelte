b9d63363b5e85ae70ea01cee80fefe3af6ee1eaa
Zorua <milandehaart@live.nl>
2024-10-09 19:41:02 +0200

    Added TextLarge component

---
+++ src/lib/components/shared/text/Large.svelte
@@ -0,0 +1,17 @@
+<script lang=ts>
+	import type { ClassType } from "$components/_types";
+	import type { Snippet } from "svelte";
+	import Text from "./Text.svelte";
+
+    let { 
+        className, 
+        children 
+    }: { className?: ClassType, children?: Snippet } = $props();
+</script>
+
+<Text
+    {className}
+    props={{ size: 'lg', weight: 'bold' }}
+>
+    {@render children?.()}
+</Text>
