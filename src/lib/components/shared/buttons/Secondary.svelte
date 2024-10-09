ac4f6a0152401520a9de702d221069a93ee86d7d
Zorua <milandehaart@live.nl>
2024-10-09 19:40:41 +0200

    Added ButtonSecondary component

---
+++ src/lib/components/shared/buttons/Secondary.svelte
@@ -0,0 +1,17 @@
+<script lang=ts>
+	import type { ClassType } from "$components/_types";
+	import type { Snippet } from "svelte";
+	import Button from "./Button.svelte";
+
+    let { 
+        className, 
+        children 
+    }: { className?: ClassType, children?: Snippet } = $props();
+</script>
+
+<Button
+    {className}
+    props={{ variant: 'secondary' }}
+>
+    {@render children?.()}
+</Button>
