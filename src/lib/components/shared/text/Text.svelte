9c2e4bd2353fd9d8b56aae2c77309990177777e5
Zorua <milandehaart@live.nl>
2024-10-09 19:41:23 +0200

    Added Text component

---
+++ src/lib/components/shared/text/Text.svelte
@@ -0,0 +1,18 @@
+<script lang=ts>
+    import type { Snippet } from "svelte";
+	import { merge } from "$lib/utils/merge";
+	import type { ClassType } from "$components/_types";
+    import { type TextProps, textVariants } from './index';
+
+    let { 
+        className,
+        props,
+        children,
+    }: { className?: ClassType, props?: TextProps, children?: Snippet } = $props();
+</script>
+
+<span
+    class={merge(textVariants({ ...props }), className)}
+>
+    {@render children?.()}
+</span>
