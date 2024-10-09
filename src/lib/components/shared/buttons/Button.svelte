43a8dc35cc6c94c31aad551a1a616bfc12f7a9aa
Zorua <milandehaart@live.nl>
2024-10-09 19:40:01 +0200

    Added Button component

---
+++ src/lib/components/shared/buttons/Button.svelte
@@ -0,0 +1,20 @@
+<script lang=ts>
+    import type { Snippet } from "svelte";
+	import { merge } from "$lib/utils/merge";
+	import type { ClassType } from "$components/_types";
+    import { type ButtonProps, buttonVariants } from './index';
+
+    let { 
+        className,
+        props,
+        children,
+    }: { className?: ClassType, props?: ButtonProps, children?: Snippet } = $props();
+</script>
+
+<button
+    class={merge(buttonVariants({ ...props }), className)}
+>
+    {#if children}
+        {@render children()}
+    {/if}
+</button>
