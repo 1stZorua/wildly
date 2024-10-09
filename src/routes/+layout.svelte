e4a3c5c271cd3ea989b222bbb1e87a48d98878a0
Zorua <milandehaart@live.nl>
2024-10-09 19:44:25 +0200

    Added wrapper and header to global layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang=ts>
+    import Header from '$components/layout/Header/Header.svelte';
+    import type { Snippet } from 'svelte';
+    import '../app.css';
+    
+    let { 
+        children 
+    }: { children: Snippet } = $props();
+</script>
+
+<div class="
+    px-16 py-8
+">
+    <Header></Header>
+    {#if children}
+        {@render children()}
+    {/if}
+</div>
