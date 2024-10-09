0d5b44eadd81b0308ce463a4dc6d760510cbce54
Zorua <milandehaart@live.nl>
2024-10-09 19:39:19 +0200

    Added header link configuration

---
+++ src/lib/components/layout/Header/config.ts
@@ -0,0 +1,23 @@
+interface Link {
+    name: string;
+    href: string;
+}
+
+export const links: Array<Link> = [
+    {
+        name: 'Home',
+        href: '/'
+    },
+    {
+        name: 'Features',
+        href: '/#'
+    },
+    {
+        name: 'About',
+        href: '/#'
+    },
+    {
+        name: 'Contact',
+        href: '/#'
+    }
+];
