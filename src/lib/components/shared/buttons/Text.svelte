5d4383d4ae797bda1b69ac09a475592a14bbc6c0
Zorua <milandehaart@live.nl>
2024-10-16 15:12:36 +0200

    Added ButtonText component

---
+++ src/lib/components/shared/buttons/Text.svelte
@@ -0,0 +1,21 @@
+<script lang=ts>
+	import type { ClassType } from "$components/_types";
+	import type { Snippet } from "svelte";
+	import Button from "./Button.svelte";
+    import type { HTMLButtonAttributes } from "svelte/elements";
+
+    interface Props extends HTMLButtonAttributes {
+        className?: ClassType;
+        children?: Snippet;
+    }
+
+    let { 
+        className, 
+        children,
+        ...rest
+    }: Props = $props();
+</script>
+
+<Button {...rest} {className} props={{ variant: "text" }}>
+    {@render children?.()}
+</Button>
