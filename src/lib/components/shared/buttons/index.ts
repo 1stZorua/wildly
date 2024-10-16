39070b95566273736a6628d376212d5e6664eb78
Zorua <milandehaart@live.nl>
2024-10-16 15:10:24 +0200

    Modified styling slightly, and added new variants: action, circle, and text

--- src/lib/components/shared/buttons/index.ts
+++ src/lib/components/shared/buttons/index.ts
@@ -1,30 +1,33 @@
-import { tv, type VariantProps } from 'tailwind-variants';
+import { tv, type VariantProps } from "tailwind-variants";
 
 const buttonVariants = tv({
-    base: 'px-9 py-3',
+    base: "md:max-w-full",
     variants: {
         variant: {
-            primary: 'border-2 border-light-btn-primary-color rounded-full',
-            secondary: '',
-            disabled: 'bg-red-500',
+            primary: "px-9 py-3 border-2 border-light-btn-primary-color rounded-full",
+            secondary: "max-w-fit px-9 py-5 bg-light-btn-secondary-background text-light-btn-secondary-color rounded-full",
+            action: "max-h-fit p-4 bg-light-btn-action-background rounded-md",
+            circle: "flex items-center justify-center p-12 bg-light-text-secondary rounded-full transition-colors",
+            text: "",
+            disabled: "bg-red-500",
         },
         size: {
-            default: '',
-            sm: '',
-            lg: '',
+            default: "",
+            sm: "",
+            lg: "",
         },
     },
     defaultVariants: {
-        variant: 'primary',
-        size: 'default',
+        variant: "primary",
+        size: "default",
     }
 })
 
 type ButtonVariants = VariantProps<typeof buttonVariants>;
 
 type Props = {
-    variant?: ButtonVariants['variant'];
-    size?: ButtonVariants['size'];
+    variant?: ButtonVariants["variant"];
+    size?: ButtonVariants["size"];
 }
 
 export {
@@ -32,6 +35,9 @@ export {
     type Props as ButtonProps
 }
 
-export { default as Button } from './Button.svelte';
-export { default as ButtonPrimary } from './Primary.svelte';
-export { default as ButtonSecondary } from './Secondary.svelte';
+export { default as Button } from "./Button.svelte";
+export { default as ButtonPrimary } from "./Primary.svelte";
+export { default as ButtonSecondary } from "./Secondary.svelte";
+export { default as ButtonAction } from "./Action.svelte";
+export { default as ButtonCircle } from "./Circle.svelte";
+export { default as ButtonText } from "./Text.svelte";
