fc88e4c04e7f32dbfd6d3b515c0378c11cf78299
Zorua <milandehaart@live.nl>
2024-10-16 15:06:18 +0200

    Restructured component for better readability, adjusted styling to match design, and updated import statements

--- src/lib/components/layout/Header/Header.svelte
+++ src/lib/components/layout/Header/Header.svelte
@@ -1,8 +1,9 @@
 <script lang=ts>
-	import { ButtonPrimary } from "$components/shared/buttons";
     import { TextBase } from "$components/shared/text";
+	import { ButtonPrimary } from "$components/shared/buttons";
+	import { Icon } from "$components/shared/other";
+    import { Dark, Light } from "$images/logo/index";
     import { links } from "./config";
-    import logo from "$lib/images/logo.svg";
 </script>
 
 {#snippet link(href: string, name: string)}
@@ -11,35 +12,22 @@
     </a>
 {/snippet}
 
-<header 
-    class="
-        flex
-        items-center
-        justify-between
-">
-    <div class="
-        flex
-        items-center
-        gap-16
-    ">
-        <img 
-            src={logo} 
-            alt="logo"
-        />
-        <ul
-            class="
-                flex
-                gap-8
-        ">
+<header class="wrapper flex items-center justify-between h-14">
+    <div class="flex items-center gap-16">
+        <img class="hidden lg:block" src={Light} alt="logo"/>
+        <img class="block lg:hidden" src={Dark} alt="logo"/>
+        <ul class="flex gap-8 xl:hidden">
             {#each links as l}
-                <li>
-                    {@render link(l.href, l.name)}
-                </li>
+                <li>{@render link(l.href, l.name)}</li>
             {/each}
         </ul>
     </div>
     
-    <ButtonPrimary>
+    <ButtonPrimary className="xl:hidden">
         <TextBase>Sign In</TextBase>
     </ButtonPrimary>
+
+    <div class="hidden xl:flex">
+        <Icon className="!text-heading lg:text-light-text-secondary" icon="lucide:menu"></Icon>
+    </div>
 </header>
