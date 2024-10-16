56691833e8724d68ee620e89b3a482e4085b1986
Zorua <milandehaart@live.nl>
2024-10-16 15:03:07 +0200

    Added configuration for the footer component

---
+++ src/lib/components/layout/footer/config.ts
@@ -0,0 +1,43 @@
+interface Section {
+    title: string,
+    contents: string[]
+}
+
+export const sections: Section[] = [
+    {
+        title: "Resources",
+        contents: [
+            "Blog",
+            "Breed guides",
+            "Dog care",
+            "Help",
+            "Sitemap"
+        ]
+    },
+    {
+        title: "Company",
+        contents: [
+            "About Wildly",
+            "Contact us",
+            "Our mission",
+            "Partners",
+            "Team"
+        ]
+    },
+    {
+        title: "Social",
+        contents: [
+            "Facebook",
+            "Instagram",
+            "Linkedin",
+            "Twitter"
+        ]
+    },
+];
+
+export const links: string[] = [
+    "&copy; 2024 Milan de Haart",
+    "Support",
+    "Privacy Policy",
+    "Cookie Policy"
+]
