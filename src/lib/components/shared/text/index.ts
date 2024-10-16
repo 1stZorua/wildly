3934ee98c6942014f12bcad3aa22b73db31e0695
Zorua <milandehaart@live.nl>
2024-10-16 15:14:21 +0200

    Modified styling slightly, and added new sizes: md, heading, and hero

--- src/lib/components/shared/text/index.ts
+++ src/lib/components/shared/text/index.ts
@@ -1,40 +1,44 @@
-import { tv, type VariantProps } from 'tailwind-variants';
+import { tv, type VariantProps } from "tailwind-variants";
 
 const textVariants = tv({
+    base: "leading-tight",
     variants: {
         weight: {
-            light: 'font-light',
-            normal: 'font-normal',
-            medium: 'font-medium',
-            semibold: 'font-semibold',
-            bold: 'font-bold',
+            light: "font-light",
+            normal: "font-normal",
+            medium: "font-medium",
+            semibold: "font-semibold",
+            bold: "font-bold",
         },
         size: {
-            xs: 'text-xs',
-            sm: 'text-sm',
-            base: 'text-base',
-            lg: 'text-lg',
-            xl: 'text-xl',
+            xs: "text-xs",
+            sm: "text-sm",
+            base: "text-base",
+            md: "text-md",
+            lg: "text-lg",
+            xl: "text-xl",
+            heading: "text-heading",
+            hero: "text-hero"
         },
         family: {
-            primary: 'font-primary',
-            secondary: 'font-secondary',
-            tertiary: 'font-tertiary'
+            primary: "font-primary",
+            secondary: "font-secondary",
+            tertiary: "font-tertiary"
         }
     },
     defaultVariants: {
-        weight: 'medium',
-        size: 'base',
-        family: 'primary'
+        weight: "medium",
+        size: "base",
+        family: "primary"
     }
 })
 
 type TextVariants = VariantProps<typeof textVariants>;
 
 type Props = {
-    weight?: TextVariants['weight'];
-    size?: TextVariants['size'];
-    family?: TextVariants['family'];
+    weight?: TextVariants["weight"];
+    size?: TextVariants["size"];
+    family?: TextVariants["family"];
 }
 
 export {
@@ -42,7 +46,10 @@ export {
     type Props as TextProps
 }
 
-export { default as Text } from './Text.svelte';
-export { default as TextSmall } from './Small.svelte';
-export { default as TextBase } from './Regular.svelte';
-export { default as TextLarge } from './Large.svelte';
+export { default as Text } from "./Text.svelte";
+export { default as TextSmall } from "./Small.svelte";
+export { default as TextBase } from "./Regular.svelte";
+export { default as TextMedium } from "./Medium.svelte";
+export { default as TextLarge } from "./Large.svelte";
+export { default as TextHeading } from "./Heading.svelte";
+export { default as TextHero } from "./Hero.svelte";
