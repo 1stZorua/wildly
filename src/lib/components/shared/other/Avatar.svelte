9c399c2f70370735335cd78897f6044bb9657ac2
Zorua <milandehaart@live.nl>
2024-10-16 15:12:44 +0200

    Added Avatar component

---
+++ src/lib/components/shared/other/Avatar.svelte
@@ -0,0 +1,15 @@
+<script lang=ts>
+    import type { ClassType } from "$components/_types";
+	import { merge } from "$lib/utils/merge";
+
+    let { 
+        className, 
+        src,
+        alt = "Avatar",
+        size = "100%"
+    }: { className?: ClassType, src: string, alt?: string, size?: string } = $props();
+</script>
+
+<div class={merge("object-cover rounded-full overflow-hidden", className)} style={`width: ${size}; height: ${size};`}>
+    <img {src} {alt} />
+</div>
