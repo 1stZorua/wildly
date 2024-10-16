66eddf42ac87ad3d461872aaa53c42f3d24ec4e6
Zorua <milandehaart@live.nl>
2024-10-16 15:08:17 +0200

    Added rest props to allow event passing

--- src/lib/components/shared/buttons/Button.svelte
+++ src/lib/components/shared/buttons/Button.svelte
@@ -2,19 +2,23 @@
     import type { Snippet } from "svelte";
 	import { merge } from "$lib/utils/merge";
 	import type { ClassType } from "$components/_types";
-    import { type ButtonProps, buttonVariants } from './index';
+    import { type ButtonProps, buttonVariants } from "./index";
+    import type { HTMLButtonAttributes } from "svelte/elements";
+
+    interface Props extends HTMLButtonAttributes {
+        className?: ClassType;
+        props?: ButtonProps;
+        children?: Snippet;
+    }
 
     let { 
         className,
         props,
         children,
-    }: { className?: ClassType, props?: ButtonProps, children?: Snippet } = $props();
+        ...rest
+    }: Props = $props();
 </script>
 
-<button
-    class={merge(buttonVariants({ ...props }), className)}
->
-    {#if children}
-        {@render children()}
-    {/if}
+<button {...rest} class={merge(buttonVariants({ ...props }), className)}>
+    {@render children?.()}
 </button>
