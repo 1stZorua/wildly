618d332aef37e7ecdeff3b31c234d86cf77d74d3
Zorua <milandehaart@live.nl>
2024-10-25 16:31:09 +0200

    Added Forest image to barrel file exports

--- src/lib/images/dogs/index.ts
+++ src/lib/images/dogs/index.ts
@@ -1,5 +1,6 @@
 export { default as Brave } from "./brave.png";
 export { default as Curious } from "./curious.png";
+export { default as Forest } from "./forest.jpg";
 export { default as Happy } from "./happy.png";
 export { default as Nature } from "./nature.jpg";
 export { default as Running } from "./running.png";
