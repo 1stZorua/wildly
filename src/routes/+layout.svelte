9c6cb5486e6e9c4afadcf94c551e2c50b5568c31
Zorua <milandehaart@live.nl>
2024-10-25 16:33:02 +0200

    Added Toaster to the layout which shows flash messages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,33 @@
 <script lang=ts>
     import { Header, Footer } from "$components/layout";
+    import { getFlash } from "sveltekit-flash-message/client";
+    import { page } from "$app/stores";
+    import { Toaster, toast } from 'svelte-sonner'
     import type { Snippet } from "svelte";
     import "iconify-icon";
     import "../app.css";
+
+    const flash = getFlash(page);
+
+    $effect(() => {
+        switch ($flash?.type) {
+            case "success":
+                toast.success($flash.message);
+                break;
+            case "error":
+                toast.error($flash.message);
+                break;
+        }
+    })
+
+    $inspect($flash?.type)
     
     let { 
         children 
     }: { children?: Snippet } = $props();
 </script>
+
+<Toaster position="top-center" richColors  />
 
 <div class="flex flex-col gap-16 pt-8 max-w-[120rem] m-auto lg:pt-4">
     <Header></Header>
