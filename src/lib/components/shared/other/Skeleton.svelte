10f1286b81c446008b1f004a1d2cabfda2095b31
Zorua <milandehaart@live.nl>
2024-10-25 16:30:50 +0200

    Added Skeleton component

---
+++ src/lib/components/shared/other/Skeleton.svelte
@@ -0,0 +1,15 @@
+<script lang=ts>
+    import type { ClassType } from "$components/_types";
+	import { merge } from "$lib/utils/merge";
+	import type { Snippet } from "svelte";
+
+    let { 
+        className, 
+        animate = true,
+        children
+    }: { className?: ClassType, animate?: boolean, children?: Snippet } = $props();
+</script>
+
+<div class={merge(`${animate ? 'bg-skeleton animate-loading bg-[length:200%_100%]' : 'bg-light-background-primary-light'}`, className)}>
+    <div class="invisible">{@render children?.()}</div>
+</div>
