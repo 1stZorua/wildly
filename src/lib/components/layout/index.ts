09728606c4dc2a8a225ba5e6b0c2bee886fd8878
Zorua <milandehaart@live.nl>
2024-10-25 16:29:07 +0200

    Added Upload component to barrel file exports

--- src/lib/components/layout/index.ts
+++ src/lib/components/layout/index.ts
@@ -4,3 +4,4 @@ export { default as Header } from "./header/Header.svelte";
 export { default as Hero } from "./hero/Hero.svelte";
 export { default as Process } from "./process/Process.svelte";
 export { default as Slogan } from "./slogan/Slogan.svelte";
+export { default as Upload } from "./upload/Upload.svelte";
