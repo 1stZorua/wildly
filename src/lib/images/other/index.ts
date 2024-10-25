9f245153a0230faf80a7c9c998f2a34c05c357ac
Zorua <milandehaart@live.nl>
2024-10-25 16:31:30 +0200

    Added border image to barrel file exports

--- src/lib/images/other/index.ts
+++ src/lib/images/other/index.ts
@@ -1,3 +1,4 @@
+export { default as Border } from "./border.png";
 export { default as Person_1 } from "./person_1.png";
 export { default as Person_2 } from "./person_2.png";
 export { default as Person_3 } from "./person_3.png";
