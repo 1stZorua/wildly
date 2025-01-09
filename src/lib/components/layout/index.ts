5e15b48504f37a597bc3401376b612d5454da1f1
Zorua <milandehaart@live.nl>
2025-01-09 15:58:28 +0100

    Temporarily commented Header component export

--- src/lib/components/layout/index.ts
+++ src/lib/components/layout/index.ts
@@ -1,6 +1,6 @@
 export { default as FAQ } from './faq/FAQ.svelte';
 export { default as Footer } from './footer/Footer.svelte';
-export { default as Header } from './header/Header.svelte';
+// export { default as Header } from './header/Header.svelte';
 export { default as Hero } from './hero/Hero.svelte';
 export { default as Process } from './process/Process.svelte';
 export { default as Slogan } from './slogan/Slogan.svelte';
