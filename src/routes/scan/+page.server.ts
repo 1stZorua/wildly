8b7e797d78ce7902c78ce25351e5510fabd09e9f
Zorua <milandehaart@live.nl>
2025-01-09 15:07:34 +0100

    Changed formatting

--- src/routes/scan/+page.server.ts
+++ src/routes/scan/+page.server.ts
@@ -4,34 +4,42 @@ import { setFlash } from 'sveltekit-flash-message/server';
 const MINIMUM_DELAY_MS = 3000;
 
 export const actions = {
-    upload: async({ request, cookies }) => {
-        const data = await request.formData();
-        const startTime = Date.now();
+	upload: async ({ request, cookies }) => {
+		const data = await request.formData();
+		const startTime = Date.now();
 
-        try {
-            const response = await fetch('http://127.0.0.1:5000/predict', {
-                method: 'POST',
-                body: data
-            });
+		try {
+			const response = await fetch('http://127.0.0.1:5000/predict', {
+				method: 'POST',
+				body: data
+			});
 
-            if (!response.ok) {
-                setFlash({ type: 'error', message: 'An error occured while making request.'}, cookies)
-                return fail(400);
-            }
+			if (!response.ok) {
+				setFlash({ type: 'error', message: 'An error occured while making request.' }, cookies);
+				return fail(400);
+			}
 
-            const result = await response.json();  
-            const elapsedTime = Date.now() - startTime;
+			const result = await response.json();
+			const elapsedTime = Date.now() - startTime;
 
-            if (elapsedTime < MINIMUM_DELAY_MS) {
-                await new Promise(resolve => setTimeout(resolve, MINIMUM_DELAY_MS - elapsedTime));
-            }
+			if (elapsedTime < MINIMUM_DELAY_MS) {
+				await new Promise((resolve) => setTimeout(resolve, MINIMUM_DELAY_MS - elapsedTime));
+			}
 
-            setFlash({ type: 'success', message: `Prediction successful.`}, cookies);
-            return { success: true, predictedBreed: result['predicted_breed'], confidence: result['confidence'], top: result['top_3'] }
-        } catch (error: unknown) {
-            setFlash({ type: 'error', message: 'An error occured while processing your request.'}, cookies)
-            console.error(error);
-            return fail(500);
-        }
-    }
-}
+			setFlash({ type: 'success', message: `Prediction successful.` }, cookies);
+			return {
+				success: true,
+				predictedBreed: result['predicted_breed'],
+				confidence: result['confidence'],
+				top: result['top_3']
+			};
+		} catch (error: unknown) {
+			setFlash(
+				{ type: 'error', message: 'An error occured while processing your request.' },
+				cookies
+			);
+			console.error(error);
+			return fail(500);
+		}
+	}
+};
