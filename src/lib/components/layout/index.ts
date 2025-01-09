18b84b3562dc8dce84beb739a91d7a8b69c3ee13
Zorua <milandehaart@live.nl>
2025-01-09 16:00:16 +0100

    Added Header export

--- src/lib/components/layout/index.ts
+++ src/lib/components/layout/index.ts
@@ -5,3 +5,4 @@ export { default as Hero } from './hero/Hero.svelte';
 export { default as Process } from './process/Process.svelte';
 export { default as Slogan } from './slogan/Slogan.svelte';
 export { default as Upload } from './upload/Upload.svelte';
+export { default as Header } from './header/Header.svelte';
